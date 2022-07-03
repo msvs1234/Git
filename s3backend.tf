bfb49f6ff6f31f6c035a0e065820a460947a02db
subbaraju <msvsraj@gmail.com>
2022-07-03 15:28:43 +0530

    added

--- s3backend.tf
+++ s3backend.tf
@@ -3,5 +3,7 @@ backend "s3" {
 bucket = "git2bucket"
 key = "git2bucket.tfstate"
 region = "us-east-1"
+access_key = "AKIAZQXRD5PP3XGJVIPZ"
+secret_key = "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"
 }
 }
